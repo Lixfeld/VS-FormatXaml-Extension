a4d80bebe4f4637420a8b196acc3a71528b78ed5
Lixfeld <lixfeld-git@gmx.de>
2022-07-03 20:39:55 +0200

    Add XamlText and Constants

---
+++ src/FormatXamlExtension/Classes/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace FormatXamlExtension.Classes
+{
+    public class Constants
+    {
+        public const string UnixLineEnding = "\n";
+        public const string WindowsLineEnding = "\r\n";
+
+        // EndTag: </...>
+        public const string OpenEndTag = "</";
+
+        // EmptyTag: <.../>
+        public const string CloseEmptyTag = "/>";
+
+        // StartTag: <...>
+        public const string OpenTag = "<";
+        public const string CloseTag = ">";
+    }
+}
---
+++ src/FormatXamlExtension/Classes/XamlText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FormatXamlExtension.Classes
+{
+    public class XamlText
+    {
+        public string Text { get; }
+        public string LineEnding { get; }
+
+        public XamlText(string text)
+        {
+            Text = text;
+
+            int lfIndex = text.IndexOf(Constants.UnixLineEnding);
+            int crlfIndex = text.IndexOf(Constants.WindowsLineEnding);
+
+            // Determine line ending by the first match
+            if (crlfIndex == -1 || crlfIndex > lfIndex)
+            {
+                LineEnding = Constants.UnixLineEnding;
+            }
+            else
+            {
+                LineEnding = Constants.WindowsLineEnding;
+            }
+        }
+    }
+}
