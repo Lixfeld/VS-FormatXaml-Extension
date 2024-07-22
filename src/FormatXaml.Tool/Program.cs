618de8133894ab4347106a13986396e477751d99
Lixfeld <lixfeld-git@gmx.de>
2024-07-22 12:33:00 +0200

    Add new console project for .NET Tool

---
+++ src/FormatXaml.Tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FormatXaml.Tool
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
